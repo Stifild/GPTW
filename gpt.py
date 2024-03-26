275cff0545114b05e0a5e760e902dca3058391fc
Stifild <140810373+Stifild@users.noreply.github.com>
2024-03-26 23:59:33 +0300

    Изменен импорт класса IOP в gpt.py и iop.py

--- gpt.py
+++ gpt.py
@@ -1,11 +1,9 @@
-import json, requests, logging
-from iop import IOP
+import json, requests, logging, time
 
-from config import MAX_ONTASK_TOKENS, FOLDER_ID, GPT_MODEL, TEMPERATURE, TOKENS_DATA_PATH, LOGS_PATH
+from config import MAX_ONTASK_TOKENS, FOLDER_ID, GPT_MODEL, TEMPERATURE, TOKENS_DATA_PATH, LOGS_PATH, IAM_TOKEN_ENDPOINT, IAM_TOKEN_PATH
 
 logging.basicConfig(filename=LOGS_PATH, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filemode="w")
 
-io = IOP()
 class GPT:
     def __init__(self):
         self.max_tokens = MAX_ONTASK_TOKENS
@@ -15,7 +13,7 @@ class GPT:
         self.tokens_data_path = TOKENS_DATA_PATH
 
     def count_tokens_in_dialogue(self, messages: list) -> int:
-        iam_token = io.get_iam_token()
+        iam_token = self.get_iam_token()
 
         headers = {
             'Authorization': f'Bearer {iam_token}',
@@ -60,7 +58,7 @@ class GPT:
 
 
     def ask_gpt(self, messages):
-        iam_token = io.get_iam_token()
+        iam_token = self.get_iam_token()
 
         url = f"https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
         headers = {
@@ -103,3 +101,45 @@ class GPT:
 
     with open(TOKENS_DATA_PATH, "r") as f:
         logging.INFO("За всё время израсходовано:", json.load(f)["tokens_count"], "токенов")
+    
+    def create_new_iam_token():
+        headers = {"Metadata-Flavor": "Google"}
+
+        try:
+            response = requests.get(IAM_TOKEN_ENDPOINT, headers=headers)
+
+        except Exception as e:
+            print("Не удалось выполнить запрос:", e)
+            print("Токен не получен")
+
+        else:
+            if response.status_code == 200:
+                token_data = {
+                    "access_token": response.json().get("access_token"),
+                    "expires_at": response.json().get("expires_in") + time.time()
+                }
+
+                with open(IAM_TOKEN_PATH, "w") as token_file:
+                    json.dump(token_data, token_file)
+
+            else:
+                print("Ошибка при получении ответа:", response.status_code)
+                print("Токен не получен")
+
+    def get_iam_token(self) -> str:
+        try:
+            with open(IAM_TOKEN_PATH, "r") as token_file:
+                token_data = json.load(token_file)
+
+            expires_at = token_data.get("expires_at")
+
+            if expires_at <= time.time():
+                self.create_new_iam_token()
+
+        except FileNotFoundError:
+            self.create_new_iam_token()
+
+        with open(IAM_TOKEN_PATH, "r") as token_file:
+            token_data = json.load(token_file)
+
+        return token_data.get("access_token")
